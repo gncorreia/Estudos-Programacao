078726df923a375589731aafb425578285d5edfb
Gabriel <gncorreia@protonmail.com>
2021-02-11 21:36:22 -0300

    Exercicios de Python e Aulas de C# e JS

--- Estudos-Python/CursoEmVideo/Ex072.py
+++ Estudos-Python/CursoEmVideo/Ex072.py
@@ -2,12 +2,16 @@
 # por extensão, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
-número = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez')
-n = int(input('Digite um número: '))
-if n == 1:
-    print(f'Você digitou o número {número[0]}')
+número = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+          'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+          'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete',
+          'dezoito', 'dezenove', 'vinte')
 
-else:
-    while n != 1:
-        print('Tente novamente.', end=' ')
-        n = int(input('Digite um número válido: '))
+n = int(input('Digite um número: '))
+while n not in range(0, 21):
+    n = int(input('Digite um número: '))
+
+for c in range(0, 21):
+    if n == c:
+        print(f'Você digitou o número {número[c]}')
+
--- Estudos-Python/CursoEmVideo/Ex073.py
+++ Estudos-Python/CursoEmVideo/Ex073.py
@@ -4,3 +4,14 @@
 # B) Os últimos 4 colocados da tabela.
 # C) Uma lista com os times em ordem alfabética.
 # D) Em que posição na tabela está o time da Chapecoense.
+
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco',
+         'Chapecoense', 'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia', 'São Paulo',
+         'Fluminense', 'Sport Recife', 'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta',
+         'Atlético-GO')
+print(f'Lista de times do Brasileirão: {times}')
+print(f'Os cinco primeiros colocados são {times[:5]}')
+print(f'Os últimos 4 colocados são {times[15:]} ')
+print(f'Os times em ordem alfabética: {sorted(times)}')
+print(f'Chapecoense está na posição {len(times[8])}')
+
