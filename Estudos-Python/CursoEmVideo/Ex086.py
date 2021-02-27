bb4cf076b74c93a5837b8b1571bbce812f0e6205
Gabriel <gncorreia@protonmail.com>
2021-02-27 04:56:11 -0300

    Execícios de Python

---
+++ Estudos-Python/CursoEmVideo/Ex086.py
@@ -0,0 +1,6 @@
+# Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores
+# lidos pelo teclado. No final, mostre a matriz na tela, com a formatação
+# correta.
+
+# faça uma lista pra cada linha, pergunte para o usuário, e dps dê print
+# nas respectivas posições da matriz
