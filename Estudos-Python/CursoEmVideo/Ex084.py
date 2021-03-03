d3e474553f085e6a97d9e8a5a69f32c277095a6f
Gabriel <gncorreia@protonmail.com>
2021-03-03 00:04:38 -0300

    Exercícios Python

--- Estudos-Python/CursoEmVideo/Ex084.py
+++ Estudos-Python/CursoEmVideo/Ex084.py
@@ -3,3 +3,35 @@
 # A) Quantas pessoas foram cadastradas.
 # B) Uma listagem com as pessoas mais pesadas.
 # C) Uma listagem com as pessoas mais leves.
+lista = []
+dados = []
+contador = pesados = leves = 0
+while True:
+    dados.append(str(input('Qual o seu nome? ')))
+    dados.append(float(input('Qual o seu peso? ')))
+    if len(lista) == 0:
+        pesados = leves = dados[1]
+    else:
+        if dados[1] > pesados:
+            pesados = dados[1]
+        if dados[1] < leves:
+            leves = dados[1]
+    lista.append(dados[:])
+    dados.clear()
+    contador += 1
+    condição = str(input('Deseja continuar? [S/N] '))
+    while condição not in 'SsNn':
+        condição = str(input('Deseja continuar? [S/N] '))
+    if condição in 'Nn':
+        break
+print('-=' * 20)
+print(f'No total, {contador} pessoas foram cadastradas.')
+print(f'O maior peso foi de {pesados}kg. Peso de ', end='')
+for pesos in lista:
+    if pesos[1] == pesados:
+        print(pesos[0])
+print(f'O menor peso foi de {leves}kg. Peso de ', end='')
+for pesos in lista:
+    if pesos[1] == leves:
+        print(pesos[0])
+
--- Estudos-Python/CursoEmVideo/Ex085.py
+++ Estudos-Python/CursoEmVideo/Ex085.py
@@ -1,3 +1,10 @@
 # Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os
 # em uma lista única que mantenha separados os valores pares e o ímpares.
 # No final, mostre os valores pares e ímpares em ordem crescente.
+lista = list()
+for num in range(0, 7):
+    n = int(input(f'Digite o {num + 1}º número: '))
+    lista.append(n)
+if lista in range(lista) % 2 == 0:
+    print(lista)
+print(lista)
