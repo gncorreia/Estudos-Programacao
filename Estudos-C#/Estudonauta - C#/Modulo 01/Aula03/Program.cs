56cd0295f86a5729448524be73b99ffe751ec150
Gabriel <76435193+gncorreia@users.noreply.github.com>
2021-03-10 18:32:48 -0300

    Aulas de C#

---
+++ Estudos-C#/Estudonauta - C#/Modulo 01/Aula03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(20, 5);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+            Console.Clear();
+            
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("\\Oi, Tudo bem?\\");
+            Console.WriteLine(@"\Oi, Tudo bem?\");
+            Console.Write("\"Olá, mundo\"");
+            Console.ResetColor();            
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Green;           
+            Console.WriteLine("C# é \n\"SUPER\"\nFácil!\a");            
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
