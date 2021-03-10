ea7f95efd3d805f679e86628a22d3d90cc56e0a7
Gabriel <76435193+gncorreia@users.noreply.github.com>
2021-03-10 17:20:44 -0300

    C# e Python

--- Estudos-Python/CursoEmVideo/Ex085.py
+++ Estudos-Python/CursoEmVideo/Ex085.py
@@ -5,6 +5,35 @@ lista = list()
 for num in range(0, 7):
     n = int(input(f'Digite o {num + 1}º número: '))
     lista.append(n)
-if lista in range(lista) % 2 == 0:
-    print(lista)
-print(lista)
+lista.sort()
+print('-='* 20)
+print(f'Os número pares são:', end=' ')
+for pares in lista:
+    if pares % 2 == 0:
+        print(pares, end=' ')
+print(' ')
+print(f'E Os número ímpares são:', end=' ')  # arrumar a ordem crescente e a formatação dos dados
+for impares in lista:
+    if impares % 2 != 0:
+        print(impares, end=' ')
+print(' ')
+print('-='* 20)
+
+# =========================
+
+numeros = [[], []]
+for num in range(0, 7):
+    n = int(input(f'Digite o {num + 1}º número: '))
+    if n % 2 == 0:
+        numeros[0].append(n)
+    else:
+        numeros[1].append(n)
+print('-=' * 20)
+numeros[0].sort()
+numeros[1].sort()
+print(f'Os números pares são: {numeros[0]}')
+print(f'Os números ímpares são: {numeros[1]}')
+print('-='* 20)
+
+
+
--- Estudos-Python/CursoEmVideo/Ex086.py
+++ Estudos-Python/CursoEmVideo/Ex086.py
@@ -4,3 +4,33 @@
 
 # faça uma lista pra cada linha, pergunte para o usuário, e dps dê print
 # nas respectivas posições da matriz
+
+linha1 = [[], [], []]
+linha2 = [[], [], []]
+linha3 = [[], [], []]
+for num in range(0, 3):
+    n = int(input(f'Digite o valor para [{0}, {num}]: '))
+    linha1[num].append(n)
+for num in range(0, 3):
+    n = int(input(f'Digite o valor para [{1}, {num}]: '))
+    linha2[num].append(n)
+for num in range(0, 3):
+    n = int(input(f'Digite o valor para [{2}, {num}]: '))
+    linha3[num].append(n)
+print('A matriz formada ficou assim:')
+print(linha1)
+print(linha2)
+print(linha3)
+
+# ---------------
+
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for linha in range(0, 3):
+    for coluna in range(0, 3): # esse segundo loop será feito três vezes. assim que completado,
+        # o primeiro loop será feito novamente e assim por diante.
+        matriz[linha][coluna] = int(input(f'Digite um número para a Linha {linha} e Coluna {coluna}: '))
+print('A matriz formada ficou assim:')
+for linha in range(0, 3):
+    for coluna in range(0,3):
+        print(f'[{matriz[linha][coluna]:^5}]', end='')
+    print() # quebra de linha
